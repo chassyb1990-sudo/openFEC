66bb5dd627665416360a1fd2985408ff2f5b3bc5
Corinne Lucas <clucas@fec.gov>
2025-11-21 14:29:22 -0500

    sync test RPS

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,2 @@
 python cli.py cf_startup
-gunicorn --access-logfile - --error-logfile - --log-level info --timeout 300 -k gevent -w 9 'webservices.rest:create_app()'
+gunicorn --access-logfile - --error-logfile - --log-level info --workers 9 --worker-class sync --timeout 300 'webservices.rest:create_app()'
